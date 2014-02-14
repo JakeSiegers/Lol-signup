a5447b136815b2c64e77a26b746971f708992cb0
Jake Siegers <sirtopeia@yahoo.com>
2014-02-13 18:41:44 -0600

    Updated bootstrap theme Added new switch code fixed display bugs

--- index.php
+++ index.php
@@ -6,69 +6,77 @@
         <meta charset="utf-8">
 
         <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-switch.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,900' rel='stylesheet' type='text/css'>
         <link href="css/style.css" rel="stylesheet">
         <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap-switch.min.js" type="text/javascript"></script>
+        <script src="js/functions.js" type="text/javascript"></script>
     </head>
     <body>
+        <div class="lol_bgDarkener">
+
+        </div>
         <div class="container lol_wrap">
             <div class="row">
-                <h1 class="lol_title">Bradley University League of Legends Tournament!<p class="lead">Sponsored by ACM - Sign up now!</p></h1>
-                <form role="form">
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Captain's Real Name">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Captain's Email">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Captain's Phone">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Name">
-                    </div>
+                <div class="col-md-12">
+                    <h1 class="lol_title">Bradley University League of Legends Tournament!<p class="lead">Sponsored by ACM - Sign up now!</p></h1>
+                    <form role="form">
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Captain's Real Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Captain's Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Captain's Phone">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Team Name">
+                        </div>
 
-                    <div class="row">
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 1">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 1">
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 2">
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 3">
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 4">
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 5">
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 2">
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 3">
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 4">
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Summoner Name 5">
-                            </div>
-                        </div>
-                    </div>
 
-
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"> I agree to the <a href="#" onclick="$('#rulesModal').modal('show'); return false;">rules</a> (Seriously, read these)
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
-                <h2>Prizes <p class="lead">(The good stuff)</p></h2>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" id="lol_checkbox"><p class="lead"> I agree to the <a href="#" onclick="$('#lol_rulesModel').modal('show'); return false;">rules</a> (Seriously, read these)</p>
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <h2>Prizes <p class="lead">(The good stuff)</p></h2>
+                </div>
             </div>
         </div>
 
-        <div class="modal fade" id="rulesModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal fade lol_rulesModel" id="lol_rulesModel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-body">
---
+++ js/functions.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jake on 2/13/14.
+ */
+$(function(){
+    $("#lol_checkbox").bootstrapSwitch();
+});
